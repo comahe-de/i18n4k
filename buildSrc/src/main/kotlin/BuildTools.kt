de847a49f90e0d3732e8093d875425fbd14cf3a0
comahe.de <78822384+comahe-de@users.noreply.github.com>
2022-05-06 22:26:51 +0200

    update BuildTools to throw an error for an unknown deployment

--- buildSrc/src/main/kotlin/BuildTools.kt
+++ buildSrc/src/main/kotlin/BuildTools.kt
@@ -21,7 +21,10 @@ object BuildTools {
         println("########### ${project.name}: Host OS is: $osName ##################")
         println("### Having publications: ")
         targetPublications.forEach { targetPublication ->
-            val isDeploy = (targetPublication.name in multiPlatformPublicationsToAllow)
+            val deployOs = multiPlatformPublicationsToAllow[targetPublication.name]
+            if (deployOs == null)
+                throw IllegalStateException("OS to deploy publication of name '${targetPublication.name}' not specified!")
+            val isDeploy = deployOs == os
             println("### * \"${targetPublication.name}\" - publish: $isDeploy")
             avoidDuplicatePublication(project, targetPublication, isDeploy)
         }
@@ -67,13 +70,16 @@ object BuildTools {
         }
     }
 
-    /** When [BuildProperties.avoidDuplicatePublications] is true, this list can be used to restrict
-     * for multi platform projects, which target should be published from which OS
-     * to avoid duplicate publications of modules that can be built on several platforms
+    /**
+     * When [BuildProperties.avoidDuplicatePublications] is true, this
+     * list can be used to restrict for multi platform projects, which
+     * target should be published from which OS to avoid duplicate
+     * publications of modules that can be built on several platforms
      *
      * What can be build on with platform:
      * https://patrickjackson.dev/publishing-multiplatform-kotlin-libraries/
      *
+     * ```
      *  |                 | Linux  | Windows | Macos |
      *  |-----------------|--------|--------|--------|
      *  | androidNative32 |   ✅   |    ✅   |   ✅   |
@@ -92,59 +98,73 @@ object BuildTools {
      *  | linuxMips32     |   ✅   |    ❌   |   ❌   |
      *  | linuxMipsel32   |   ✅   |    ❌   |   ❌   |
      *  | linuxX64        |   ✅   |    ✅   |   ✅   |
+     *  ```
      */
-    val multiPlatformPublicationsToAllow: List<String>
-        get() = when {
-            isWindows -> listOf(
-                "mingwX64",
-                "mingwx86"
-            )
-            isLinux -> listOf(
-                "kotlinMultiplatform",
-                "metadata",
-                "jvm",
-                "js",
-                "linuxArm32Hfp",
-                "linuxArm64",
-                "linuxX64",
-                "linuxMips32",
-                "linuxMipsel32",
-                "androidNativeArm32",
-                "androidNativeArm64",
-                "wasm32",
-            )
-            isMacOS -> listOf(
-                "macosX64",
-                "macosArm64",
-                "iosX64",
-                "iosArm32",
-                "iosArm64",
-                "iosSimulatorArm64",
-                "watchosArm32",
-                "watchosArm64",
-                "watchosX86",
-                "watchosX64",
-                "watchosSimulatorArm64",
-                "tvosArm64",
-                "tvosX64",
-                "tvosSimulatorArm64",
-            )
-            else -> emptyList()
-        }
+
+    val multiPlatformPublicationsToAllow: Map<String, OperationSysten> =
+        mapOf(
+            // Windows
+            "mingwX64" to OperationSysten.WINDOWS,
+            "mingwx86" to OperationSysten.WINDOWS,
+            // Linux
+            "kotlinMultiplatform" to OperationSysten.LINUX,
+            "metadata" to OperationSysten.LINUX,
+            "jvm" to OperationSysten.LINUX,
+            "js" to OperationSysten.LINUX,
+            "linuxArm32Hfp" to OperationSysten.LINUX,
+            "linuxArm64" to OperationSysten.LINUX,
+            "linuxX64" to OperationSysten.LINUX,
+            "linuxMips32" to OperationSysten.LINUX,
+            "linuxMipsel32" to OperationSysten.LINUX,
+            "androidNativeArm32" to OperationSysten.LINUX,
+            "androidNativeArm64" to OperationSysten.LINUX,
+            "wasm32" to OperationSysten.LINUX,
+            // MacOS
+            "macosX64" to OperationSysten.MACOS,
+            "macosArm64" to OperationSysten.MACOS,
+            "iosX64" to OperationSysten.MACOS,
+            "iosArm32" to OperationSysten.MACOS,
+            "iosArm64" to OperationSysten.MACOS,
+            "iosSimulatorArm64" to OperationSysten.MACOS,
+            "watchosArm32" to OperationSysten.MACOS,
+            "watchosArm64" to OperationSysten.MACOS,
+            "watchosX86" to OperationSysten.MACOS,
+            "watchosX64" to OperationSysten.MACOS,
+            "watchosSimulatorArm64" to OperationSysten.MACOS,
+            "tvosArm64" to OperationSysten.MACOS,
+            "tvosX64" to OperationSysten.MACOS,
+            "tvosSimulatorArm64" to OperationSysten.MACOS,
+        )
+
 
     /** Nome of the current operating system */
     val osName: String?
         get() = System.getProperty("os.name")
 
-    /** Is the current operating system Windows  */
+    val os: OperationSysten
+        get() {
+            if (isWindows) return OperationSysten.WINDOWS
+            if (isLinux) return OperationSysten.LINUX
+            if (isMacOS) return OperationSysten.MACOS
+            return OperationSysten.UNKNOWN
+        }
+
+    /** Is the current operating system Windows */
     val isWindows: Boolean
         get() = osName?.startsWith("Windows") ?: false
 
-    /** Is the current operating system Linux  */
+    /** Is the current operating system Linux */
     val isLinux: Boolean
         get() = osName?.startsWith("Linux") ?: false
 
-    /** Is the current operating system MacOS  */
+    /** Is the current operating system MacOS */
     val isMacOS: Boolean
         get() = osName?.startsWith("Mac OS") ?: false
+
+    enum class OperationSysten {
+        WINDOWS,
+        LINUX,
+        MACOS,
+        UNKNOWN
+    }
 }
