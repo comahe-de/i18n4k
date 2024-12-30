c5435fda21b59737d746625cfd0fad00e8043a67
comahe.de <78822384+comahe-de@users.noreply.github.com>
2024-12-30 22:56:25 +0100

    [#72] appleMain: Use `NSLocale.autoupdatingCurrentLocale` in `systemLocale`

--- i18n4k-core/src/appleMain/kotlin/de.comahe.i18n4k/Locale.kt
+++ i18n4k-core/src/appleMain/kotlin/de.comahe.i18n4k/Locale.kt
@@ -2,13 +2,13 @@ package de.comahe.i18n4k
 
 import platform.Foundation.NSLocale
 import platform.Foundation.countryCode
-import platform.Foundation.currentLocale
+import platform.Foundation.autoupdatingCurrentLocale
 import platform.Foundation.languageCode
 import platform.Foundation.variantCode
 
 actual val systemLocale: Locale
     get() {
-        val locale = NSLocale.currentLocale
+        val locale = NSLocale.autoupdatingCurrentLocale
         return Locale(
             language = locale.languageCode,
             country = locale.countryCode.orEmpty(),
