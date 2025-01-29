053c43ab63057580f142e4453e07fa4311c51a6e
DatLag <jeff.retz08@gmail.com>
2025-01-29 20:19:46 +0100

    get system locale on linux

---
+++ i18n4k-core/src/linuxMain/kotlin/de/comahe/i18n4k/Locale.kt
@@ -0,0 +1,20 @@
+package de.comahe.i18n4k
+
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.toKString
+import platform.posix.getenv
+
+@OptIn(ExperimentalForeignApi::class)
+actual val systemLocale: Locale
+    get() {
+        val full = getenv("LANG")?.toKString()?.substringBefore('.')?.ifBlank { null }
+        val lang = full?.substringBefore('_')?.takeIf { it.length in 2..3 }?.ifBlank { null }
+        val country = full?.substringAfter('_')?.takeIf { it.length in 2..3 }?.ifBlank { null }
+
+        return lang?.let {
+            createLocale(
+                language = it,
+                country = country,
+            )
+        } ?: Locale("en")
+    }
