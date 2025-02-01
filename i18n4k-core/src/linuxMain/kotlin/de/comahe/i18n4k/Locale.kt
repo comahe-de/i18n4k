8efb8343bf706082670ff6847315ed2d24fbf68c
comahe.de <78822384+comahe-de@users.noreply.github.com>
2025-02-01 23:32:05 +0100

    [#75] Catch errors in `systemLocale` on Linux & Android

--- i18n4k-core/src/linuxMain/kotlin/de/comahe/i18n4k/Locale.kt
+++ i18n4k-core/src/linuxMain/kotlin/de/comahe/i18n4k/Locale.kt
@@ -7,9 +7,18 @@ import platform.posix.getenv
 @OptIn(ExperimentalForeignApi::class)
 actual val systemLocale: Locale
     get() {
-        val tag = getenv("LANG")?.toKString()?.substringBefore('.')?.trim()?.ifBlank { null }
+        var locale: Locale? = null
+        try {
+            val tag = getenv("LANG")?.toKString()?.substringBefore('.')?.trim()?.ifBlank { null }
 
-        return tag?.let(::forLocaleTag)?.takeUnless {
-            it.getLanguage().isBlank() && it.getCountry().isBlank()
-        } ?: Locale("en")
+            locale = tag?.let(::forLocaleTag)?.takeUnless {
+                it.getLanguage().isBlank() && it.getCountry().isBlank()
+            }
+        } catch (ignore: Throwable) {
+            //probably an invalid tag, like "C"
+        }
+        if (locale == null) {
+            locale = Locale("en")
+        }
+        return locale
     }
