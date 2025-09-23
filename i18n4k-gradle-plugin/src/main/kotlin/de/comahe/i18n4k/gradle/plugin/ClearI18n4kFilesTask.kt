b5ee904c13678a9121bb7a5a75abc06c29dcf50b
comahe.de <78822384+comahe-de@users.noreply.github.com>
2025-09-23 22:59:21 +0200

    [#81] plugin: support for Gradle configuration cache

--- i18n4k-gradle-plugin/src/main/kotlin/de/comahe/i18n4k/gradle/plugin/ClearI18n4kFilesTask.kt
+++ i18n4k-gradle-plugin/src/main/kotlin/de/comahe/i18n4k/gradle/plugin/ClearI18n4kFilesTask.kt
@@ -1,8 +1,10 @@
 package de.comahe.i18n4k.gradle.plugin
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
+import java.io.File
 
 
 @Suppress("unused")
@@ -11,10 +13,17 @@ open class ClearI18n4kFilesTask : DefaultTask() {
     @Internal
     lateinit var config: I18n4kExtension
 
+    @get:Input
+    val projectDirectory: String = project.layout.projectDirectory.asFile.absolutePath
+
+    @get:Input
+    val buildDirectory: String = project.layout.buildDirectory.get().asFile.absolutePath
+
+
     @TaskAction
     fun doIt() {
-        I18n4kPlugin.getGeneratedSourcesDirectory(project, config).deleteRecursively()
-        I18n4kPlugin.getGeneratedLanguageFilesDirectory(project, config).deleteRecursively()
+        I18n4kPlugin.getGeneratedSourcesDirectory(File(buildDirectory), config).deleteRecursively()
+        I18n4kPlugin.getGeneratedLanguageFilesDirectory(File(buildDirectory), File(projectDirectory), config).deleteRecursively()
     }
 
 }
